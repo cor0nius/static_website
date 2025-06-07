602ecd4395c466c4b54c773adacac5e8f490af93
cor0nius <jacekglowacki94@gmail.com>
2025-06-07 13:58:49 +0200

    updated main

--- src/main.py
+++ src/main.py
@@ -5,4 +5,14 @@ def main():
     node = TextNode("sometext", "link", "thisisan.url")
     print(node)
 
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+        elif delimiter not in ["**", "_", "`", "[", "![", "]"]:
+            raise Exception("unknown text delimiter")
+        else:
+            
+
 main()
